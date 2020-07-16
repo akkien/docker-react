59046b9ba7e1e5f05b6b0654cac82d7a5a7a6228
le trung kien <akkien99@gmail.com>
2020-07-16 09:07:39 +0700

    add rsa key

---
+++ scripts/deploy.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# print outputs and exit on first failure
+set -xe
+
+if [ $TRAVIS_BRANCH == "master" ] ; then
+
+    # # setup ssh agent, git config and remote
+    # eval "$(ssh-agent -s)"
+    # ssh-add ~/.ssh/travis_rsa
+    # git remote add deploy "travis@webhost.planecq.xyz:/var/www/planecq.com"
+    # git config user.name "Le Trung Kien"
+    # git config user.email "akkien99@gmail.com"
+
+    # # commit compressed files and push it to remote
+    # rm -f .gitignore
+    # git add .
+    # git status # debug
+    # git commit -m "Deploy compressed files"
+    # git push -f deploy HEAD:master
+
+    yarn start
+
+else
+    echo "No deploy script for branch '$TRAVIS_BRANCH'"
+fi
