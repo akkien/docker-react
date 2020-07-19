8039c8287765111c8c6e6e4b36040e926e5992c3
le trung kien <akkien99@gmail.com>
2020-07-19 20:21:46 +0700

    travis push docker image

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,11 @@ set -xe
 if [ $TRAVIS_BRANCH == "master" ] ; then
 
     whoami
-    echo "$DOCKER_USER"
+
+    docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
+    docker build -t $DOCKER_IMAGE .
+    docker push $DOCKER_IMAGE
+    
     ssh $REMOTE_USER@$REMOTE_HOST 'bash -s' < ./scripts/onvps.sh
 
 else
--- scripts/onvps.sh
+++ scripts/onvps.sh
@@ -2,16 +2,17 @@
 
 # print outputs and exit on first failure
 set -xe
+
 whoami
-echo "$DOCKER_USER"
-# docker login -u="$DOCKER_USER" -p="$DOCKER_PASS"
 
-# if [ "$(docker ps -q -f name=reactjs)" ]; then
-#   docker stop reactjs
-#   docker rm reactjs
-#   if [ "$(docker ps -aq -f status=exited -f name=reactjs)" ]; then
-#       docker rm reactjs
-#   fi  
-# fi
+docker login
 
-# docker run -d -p 80:80 --name reactjs akkien/docker-react
+if [ "$(docker ps -q -f name=reactjs)" ]; then
+  docker stop reactjs
+  docker rm reactjs
+  if [ "$(docker ps -aq -f status=exited -f name=reactjs)" ]; then
+      docker rm reactjs
+  fi  
+fi
+
+docker run -d -p 80:80 --name reactjs akkien/docker-react
