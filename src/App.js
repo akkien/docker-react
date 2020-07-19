719f6f128fa9d62efe97823bfe1a78500f77e003
le trung kien <akkien99@gmail.com>
2020-07-19 20:39:01 +0700

    make changed to check if ci/ci working

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>My first travis-ci on Digital Ocean. User deployer</p>
+        <p>My first travis-ci on Digital Ocean. Repo changed</p>
         <a
           className='App-link'
           href='https://reactjs.org'
